b16fdfaf520a5688e8f9f633391398982851fa05
ShubhamCodes <shubhambassi52@gmail.com>
2020-05-28 19:50:55 +1000

    second commit

--- src/app/icons/icons.component.ts
+++ src/app/icons/icons.component.ts
@@ -1,4 +1,4 @@
-  import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, throwError } from "rxjs";
 import { map, filter } from "rxjs/operators";
